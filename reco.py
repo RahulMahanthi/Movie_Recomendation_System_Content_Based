d32a79bf028c4f020911c7d9303d207c0afdafa5
RahulMahanthi <rahulkomal834@gmail.com>
2024-09-07 23:36:54 +0530

    Updated

--- reco.py
+++ reco.py
@@ -1,187 +1,293 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# movies=pd.read_csv("movies.csv")
+# credits=pd.read_csv("credits.csv")
+
+# movies.head()
+
+# credits.head()
+
+# movies.shape
+
+# credits.shape
+
+# movies=movies.merge(credits, on ='title')
+
+# movies.head()
+
+# movies.shape
+
+# movies['original_language'].value_counts()
+
+# movies.columns
+
+# movies=movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+# movies.head()
+
+# movies.shape
+
+# movies.dtypes
+
+# movies.isnull()
+
+# movies.isnull().sum()
+
+# movies.dropna(inplace=True)
+
+# movies.isnull().sum()
+
+# movies.shape
+
+# movies.duplicated()
+
+# movies.duplicated().sum()
+
+# movies.head()
+
+# movies.iloc[0]['genres']
+
+# import ast
+
+# ast.literal_eval('[{"id": 28, "name": "Action"}, {"id": 12, "name": "Adventure"}, {"id": 14, "name": "Fantasy"}, {"id": 878, "name": "Science Fiction"}]')
+
+# def convert(text):
+#     l=[]
+#     for i in ast.literal_eval(text):
+#         l.append(i['name'])
+#     return l
+
+# movies['genres']=movies['genres'].apply(convert)
+
+# movies['genres']
+
+# movies.head()
+
+# movies['keywords']=movies['keywords'].apply(convert)
+
+# movies
+
+# def convert_cast(text):
+#     l=[]
+#     counter=0
+#     for i in ast.literal_eval(text):
+#         if counter < 3:
+#             l.append(i['name'])
+#         counter+=1
+#     return l
+
+# movies['cast']=movies['cast'].apply(convert_cast)
+
+# movies['cast']
+
+# movies
+
+# def fetch_convert(text):
+#     l=[]
+#     for i in ast.literal_eval(text):
+#         if i['job']=='Director':
+#             l.append(i['name'])
+#             break
+#     return l
+
+# movies['crew']=movies['crew'].apply(fetch_convert)
+
+# movies['crew']
+
+# movies.head()
+
+# movies['overview']=movies['overview'].apply(lambda x:x.split())
+
+# movies
+
+# def remove_space(word):
+#     l=[]
+#     for i in word:
+#         l.append(i.replace(" ",""))
+#     return l
+
+# movies['cast']=movies['cast'].apply(remove_space).copy()
+
+# movies['crew']=movies['crew'].apply(remove_space).copy()
+# movies['keywords']=movies['keywords'].apply(remove_space).copy()
+# movies['genres']=movies['genres'].apply(remove_space).copy()
+
+# movies
+
+# movies['tags']=movies['genres']+movies['overview']+movies['cast']+movies['crew']+movies['keywords']
+
+# movies
+
+# movies['tags']
+
+# new_data=movies[['movie_id','title','tags']]
+
+# new_data
+
+# new_data['tags']=new_data['tags'].apply(lambda x: " ".join(x)).copy()
+
+# new_data
+
+# movies['tags']
+
+# new_data['tags']=new_data['tags'].apply(lambda x:x.lower()).copy()
+
+# new_data
+
+# new_data.shape
+
+# import nltk
+# from nltk.stem import PorterStemmer
+
+# ps=PorterStemmer()
+
+# def stems(text):
+#     l=[]
+#     for i in text.split():
+#         l.append(ps.stem(i))
+#     return " ".join(l)
+
+# new_data['tags']=new_data['tags'].apply(stems).copy()
+
+# new_data['tags']
+
+# from sklearn.feature_extraction.text import CountVectorizer
+
+# cv=CountVectorizer(max_features=5000,stop_words='english')
+
+# vector=cv.fit_transform(new_data['tags']).toarray()
+
+# vector
+
+# vector.shape
+
+# from sklearn.metrics.pairwise import cosine_similarity
+
+# similarity=cosine_similarity(vector)
+
+# similarity
+
+# similarity.shape
+
+# new_data[new_data['title']=='Spider-Man'].index[0]
+
+# def recommend(movie):
+#     index=new_data[new_data['title']==movie].index[0]
+#     distances=sorted(list(enumerate(similarity[index])),reverse=True, key= lambda x:x[1])
+#     for i in distances[1:6]:
+#         print(new_data.iloc[i[0]].title)
+
+# recommend('The Dark Knight Rises')
+
+# import pickle
+
+# pickle.dump(new_data,open('artificates/movie_list.pkl','wb'))
+# pickle.dump(similarity,open('artificates/movie_similarity.pkl','wb'))
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-movies=pd.read_csv("movies.csv")
-credits=pd.read_csv("credits.csv")
-
-movies.head()
-
-credits.head()
-
-movies.shape
-
-credits.shape
-
-movies=movies.merge(credits, on ='title')
-
-movies.head()
-
-movies.shape
-
-movies['original_language'].value_counts()
-
-movies.columns
-
-movies=movies[['movie_id','title','overview','genres','keywords','cast','crew']]
-
-movies.head()
-
-movies.shape
-
-movies.dtypes
-
-movies.isnull()
-
-movies.isnull().sum()
-
-movies.dropna(inplace=True)
-
-movies.isnull().sum()
-
-movies.shape
-
-movies.duplicated()
-
-movies.duplicated().sum()
-
-movies.head()
-
-movies.iloc[0]['genres']
-
+import nltk
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
 import ast
 
-ast.literal_eval('[{"id": 28, "name": "Action"}, {"id": 12, "name": "Adventure"}, {"id": 14, "name": "Fantasy"}, {"id": 878, "name": "Science Fiction"}]')
+# Reading datasets
+movies = pd.read_csv("movies.csv")
+credits = pd.read_csv("credits.csv")
 
+# Merging movies and credits datasets
+movies = movies.merge(credits, on='title')
+
+# Keeping relevant columns
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+
+# Handling missing values
+movies.dropna(inplace=True)
+
+# Converting genres, keywords, cast, crew into list of names
 def convert(text):
-    l=[]
+    l = []
     for i in ast.literal_eval(text):
         l.append(i['name'])
     return l
 
-movies['genres']=movies['genres'].apply(convert)
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
 
-movies['genres']
-
-movies.head()
-
-movies['keywords']=movies['keywords'].apply(convert)
-
-movies
-
+# Limiting cast to top 3 actors
 def convert_cast(text):
-    l=[]
-    counter=0
+    l = []
+    counter = 0
     for i in ast.literal_eval(text):
         if counter < 3:
             l.append(i['name'])
-        counter+=1
+        counter += 1
     return l
 
-movies['cast']=movies['cast'].apply(convert_cast)
+movies['cast'] = movies['cast'].apply(convert_cast)
 
-movies['cast']
-
-movies
-
+# Fetching director from crew
 def fetch_convert(text):
-    l=[]
+    l = []
     for i in ast.literal_eval(text):
-        if i['job']=='Director':
+        if i['job'] == 'Director':
             l.append(i['name'])
             break
     return l
 
-movies['crew']=movies['crew'].apply(fetch_convert)
+movies['crew'] = movies['crew'].apply(fetch_convert)
 
-movies['crew']
+# Splitting overview into words
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
 
-movies.head()
-
-movies['overview']=movies['overview'].apply(lambda x:x.split())
-
-movies
-
+# Removing spaces in names
 def remove_space(word):
-    l=[]
+    l = []
     for i in word:
-        l.append(i.replace(" ",""))
+        l.append(i.replace(" ", ""))
     return l
 
-movies['cast']=movies['cast'].apply(remove_space).copy()
+movies['cast'] = movies['cast'].apply(remove_space)
+movies['crew'] = movies['crew'].apply(remove_space)
+movies['keywords'] = movies['keywords'].apply(remove_space)
+movies['genres'] = movies['genres'].apply(remove_space)
 
-movies['crew']=movies['crew'].apply(remove_space).copy()
-movies['keywords']=movies['keywords'].apply(remove_space).copy()
-movies['genres']=movies['genres'].apply(remove_space).copy()
+# Creating tags by combining relevant columns
+movies['tags'] = movies['genres'] + movies['overview'] + movies['cast'] + movies['crew'] + movies['keywords']
+new_data = movies[['movie_id', 'title', 'tags']]
 
-movies
+# Converting tags to string and lowercasing
+new_data['tags'] = new_data['tags'].apply(lambda x: " ".join(x).lower())
 
-movies['tags']=movies['genres']+movies['overview']+movies['cast']+movies['crew']+movies['keywords']
-
-movies
-
-movies['tags']
-
-new_data=movies[['movie_id','title','tags']]
-
-new_data
-
-new_data['tags']=new_data['tags'].apply(lambda x: " ".join(x)).copy()
-
-new_data
-
-movies['tags']
-
-new_data['tags']=new_data['tags'].apply(lambda x:x.lower()).copy()
-
-new_data
-
-new_data.shape
-
-import nltk
-from nltk.stem import PorterStemmer
-
-ps=PorterStemmer()
+# Applying stemming to tags
+ps = PorterStemmer()
 
 def stems(text):
-    l=[]
+    l = []
     for i in text.split():
         l.append(ps.stem(i))
     return " ".join(l)
 
-new_data['tags']=new_data['tags'].apply(stems).copy()
+new_data['tags'] = new_data['tags'].apply(stems)
 
-new_data['tags']
+# Creating count vectorizer model
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vector = cv.fit_transform(new_data['tags']).toarray()
 
-from sklearn.feature_extraction.text import CountVectorizer
+# Calculating cosine similarity
+similarity = cosine_similarity(vector)
 
-cv=CountVectorizer(max_features=5000,stop_words='english')
-
-vector=cv.fit_transform(new_data['tags']).toarray()
-
-vector
-
-vector.shape
-
-from sklearn.metrics.pairwise import cosine_similarity
-
-similarity=cosine_similarity(vector)
-
-similarity
-
-similarity.shape
-
-new_data[new_data['title']=='Spider-Man'].index[0]
-
+# Movie recommendation function
 def recommend(movie):
-    index=new_data[new_data['title']==movie].index[0]
-    distances=sorted(list(enumerate(similarity[index])),reverse=True, key= lambda x:x[1])
+    index = new_data[new_data['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     for i in distances[1:6]:
         print(new_data.iloc[i[0]].title)
 
-recommend('The Dark Knight Rises')
-
-import pickle
-
-pickle.dump(new_data,open('artificates/movie_list.pkl','wb'))
-pickle.dump(similarity,open('artificates/movie_similarity.pkl','wb'))
+# Saving the model and data
+pickle.dump(new_data, open('artificates/movie_list.pkl', 'wb'))
+pickle.dump(similarity, open('artificates/movie_similarity.pkl', 'wb'))
